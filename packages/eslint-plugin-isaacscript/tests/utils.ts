347c0ce7949eec71c228c187c1a576ef90715411
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-05-01 00:21:53 -0400

    fix(plugin): jest

--- packages/eslint-plugin-isaacscript/tests/utils.ts
+++ packages/eslint-plugin-isaacscript/tests/utils.ts
@@ -1,10 +1,11 @@
 import { RuleTester } from "@typescript-eslint/rule-tester";
 import path from "node:path";
 
+/** @see https://typescript-eslint.io/packages/rule-tester */
 export const ruleTester = new RuleTester({
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    tsconfigRootDir: path.join(import.meta.dirname, "fixtures"),
     project: true,
-    tsconfigRootDir: path.join(__dirname, "fixtures"),
   },
 });
